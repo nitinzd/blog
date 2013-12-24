f01be63bb97eb119d496f350aae45b0713bd0bd9
Nitin Dhaware <nitin@techvision.net.in>
2013-12-24 13:24:30 +0530

    Have added error links and aria attributes

--- app/assets/javascripts/posts.js.coffee
+++ app/assets/javascripts/posts.js.coffee
@@ -1,3 +1,14 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+  $(document).ready ->
+    error_inputs = $(".field_with_errors :input")
+    $.each error_inputs, (i, field)->
+      attr = $(field).attr('id')
+      $(".error_link[field='#{attr}']").click ->
+        $(field).focus()
+
+
+
+
--- app/models/post.rb
+++ app/models/post.rb
@@ -10,5 +10,33 @@ include Mongoid::Attributes::Dynamic
   field :select_tag, type: String
   field :publish, type: Boolean
   field :post_number, type: Integer
-validates :title, :author, :presence => true
+  validates :title, :author, :presence => true
+  validates :email, uniqueness: true
+  validates :body, length: {minimum: 100, maximum: 500}
+  validate :checkDate
+
+    def valid_date(date)
+regexp = /\d{2}\/\d{2}\/\d{4}/
+date = date.to_s
+    if date.include?("/")
+      arr_date = date.split("/")
+    elsif date.include?("-")
+            arr_date = date.split("-")
+    end
+    if regexp.match(date) != nil
+      if arr_date != nil && arr_date.length == 3
+        if Date.valid_date?(arr_date[2].to_i, arr_date[1].to_i, arr_date[0].to_i)
+          return true
+        end
+      end
+    end
+    return false
+  end
+
+  def checkDate
+    if valid_date(created_date) == false
+      errors.add(:created_date, "Invalid")
+    end
+  end
+
 end
